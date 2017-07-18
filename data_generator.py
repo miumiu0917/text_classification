c0215ef3712466818ff4c9d6df74a13c0c202299
miumiu0917 <miumiu917@gmail.com>
2017-07-18 06:15:49 +0000

    データ生成

--- data_generator.py
+++ data_generator.py
@@ -1,4 +1,50 @@
+import mojimoji
+import random
+import csv
 
+
+CATEGORY_DICT = {
+  0: [0, 500]
+  , 1: [500, 1000]
+  , 2: [1000, 4999]
+  , 3: [5000, 10000]
+  , 4: [10000, 50000]
+  , 5: [50000, 100000]
+  }
+
+
+def main():
+  first = read_from_file('data/candidate/first.txt')
+  last = read_from_file('data/candidate/last.txt')
+  money_first = read_from_file('data/candidate/money_first.txt')
+  money_last = read_from_file('data/candidate/money_last.txt')
+  with open('data/raw.csv', 'w') as f:
+    writer = csv.writer(f, lineterminator='\n')
+    for _ in range(50000):
+      category, money_ = money()
+      text = random.choice(first) + random.choice(money_first) + money_ + random.choice(money_last) + random.choice(last)
+      writer.writerow([category, text])
+
+def money():
+  category = random.choice(list(CATEGORY_DICT.keys()))
+  range_ = CATEGORY_DICT[category]
+  return category, convert(str(random.randrange(range_[0], range_[1])))
+
+def read_from_file(file_path):
+  with open(file_path, 'r') as f:
+    return [line.rstrip("\n") for line in f]
+
+def convert(money):
+  fifty_fifty = lambda: random.randint(0,1) % 2 == 0
+  comma = lambda s: "{:,}".format(int(money))
+  han2zen = lambda s: mojimoji.han_to_zen(s)
+  unit = lambda s: s + '円' if fifty_fifty() else '￥' + s
+  if fifty_fifty():
+    money = comma(money)
+  if fifty_fifty():
+    money = han2zen(money)
+  money = unit(money)
+  return money
 
 
 if __name__ == '__main__':
